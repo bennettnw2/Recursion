87321aa03ebc2c37fb85be57d2ccaa6b0d28725c
Nygel Bennett <nygel.bennett@gmail.com>
2019-10-02 14:57:40 -0400

    Add files via upload

---
+++ Recursion2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Recursion2 {
+  public static void main(String [] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println( "Enter first number" );
+    int x = input.nextInt();
+    System.out.println( "Enter second number" );
+    int y = input.nextInt();
+    addTwoNumbers(x,y);
+
+  }
+
+  public static int addTwoNumbers(int x, int y) {
+    System.out.println( "x is " + x + " and y is " + y);
+
+    while (x != 0) {
+      // x -= 1;
+      // y += 1;
+      addTwoNumbers(x -= 1, y += 1);
+      // System.out.println( "x is " + x + " and y is " + y);
+      return x;
+    }
+
+    //System.out.println( "x is " + x + " and y is " + y);
+    return x;
+  }
+
+} 
